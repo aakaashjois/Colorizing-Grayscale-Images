48d0176b5d1699e89d00b347df0b6cc3da0f59e1
Aakaash Jois <aakaashjois@gmail.com>
2018-05-11 16:46:08 -0400

    Add Deep Koalarization model

---
+++ src/Deep-Koalarization.py
@@ -0,0 +1,90 @@
+import keras
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.preprocessing import image
+from keras.engine import Layer
+from keras.applications.inception_resnet_v2 import preprocess_input
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose, Input, Reshape, merge, concatenate
+from keras.layers import Activation, Dense, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import TensorBoard 
+from keras.models import Sequential, Model
+from keras.layers.core import RepeatVector, Permute
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb, rgb2gray, gray2rgb
+from skimage.transform import resize
+from skimage.io import imsave
+import numpy as np
+import os
+import random
+import tensorflow as tf
+from tqdm import tqdm
+
+#Load weights
+inception = InceptionResNetV2(weights='imagenet', include_top=True)
+inception.graph = tf.get_default_graph()
+
+embed_input = Input(shape=(1000,))
+
+#Encoder
+encoder_input = Input(shape=(256, 256, 1,))
+encoder_output = Conv2D(64, (3,3), activation='relu', padding='same', strides=2)(encoder_input)
+encoder_output = Conv2D(128, (3,3), activation='relu', padding='same')(encoder_output)
+encoder_output = Conv2D(128, (3,3), activation='relu', padding='same', strides=2)(encoder_output)
+encoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+encoder_output = Conv2D(256, (3,3), activation='relu', padding='same', strides=2)(encoder_output)
+encoder_output = Conv2D(512, (3,3), activation='relu', padding='same')(encoder_output)
+encoder_output = Conv2D(512, (3,3), activation='relu', padding='same')(encoder_output)
+encoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+
+#Fusion
+fusion_output = RepeatVector(32 * 32)(embed_input) 
+fusion_output = Reshape(([32, 32, 1000]))(fusion_output)
+fusion_output = concatenate([encoder_output, fusion_output], axis=3) 
+fusion_output = Conv2D(256, (1, 1), activation='relu', padding='same')(fusion_output) 
+
+#Decoder
+decoder_output = Conv2D(128, (3,3), activation='relu', padding='same')(fusion_output)
+decoder_output = UpSampling2D((2, 2))(decoder_output)
+decoder_output = Conv2D(64, (3,3), activation='relu', padding='same')(decoder_output)
+decoder_output = UpSampling2D((2, 2))(decoder_output)
+decoder_output = Conv2D(32, (3,3), activation='relu', padding='same')(decoder_output)
+decoder_output = Conv2D(16, (3,3), activation='relu', padding='same')(decoder_output)
+decoder_output = Conv2D(2, (3, 3), activation='tanh', padding='same')(decoder_output)
+decoder_output = UpSampling2D((2, 2))(decoder_output)
+
+model = Model(inputs=[encoder_input, embed_input], outputs=decoder_output)
+
+def create_inception_embedding(grayscaled_rgb):
+    grayscaled_rgb_resized = []
+    for i in grayscaled_rgb:
+        i = resize(i, (299, 299, 3), mode='constant')
+        grayscaled_rgb_resized.append(i)
+    grayscaled_rgb_resized = np.array(grayscaled_rgb_resized)
+    grayscaled_rgb_resized = preprocess_input(grayscaled_rgb_resized)
+    with inception.graph.as_default():
+        embed = inception.predict(grayscaled_rgb_resized)
+    return embed
+
+# Image transformer
+datagen = ImageDataGenerator()
+
+#Generate training data
+batch_size = 64
+
+def image_a_b_gen(batch_size):
+    for batch in datagen.flow_from_directory('./data/train/', target_size=(256, 256), batch_size=batch_size, class_mode=None):
+        batch = np.divide(batch, 255)
+        grayscaled_rgb = gray2rgb(rgb2gray(batch))
+        embed = create_inception_embedding(grayscaled_rgb)
+        lab_batch = rgb2lab(batch)
+        X_batch = lab_batch[:,:,:,0]
+        X_batch = X_batch.reshape(X_batch.shape+(1,))
+        Y_batch = lab_batch[:,:,:,1:] / 128
+        yield ([X_batch, embed], Y_batch)
+
+
+#Train model      
+model.compile(optimizer='adam', loss='mse')
+for e in range(100):
+    model.fit_generator(image_a_b_gen(batch_size), epochs=1, steps_per_epoch=160, verbose=1)
+model.save('Deep-Koalarization-model.h5')
